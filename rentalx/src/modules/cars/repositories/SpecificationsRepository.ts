e17c15e50443b87f4969996d048cea5c21c8e9ea
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-19 15:10:10 -0300

    feat: create SpecificationsRepository

---
+++ rentalx/src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,35 @@
+import { Specification } from '../models/Specification';
+import {
+  ICreateSpecificationDTO,
+  ISpecificationsRepository,
+} from './ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO) {
+    const specificaiton = new Specification();
+
+    Object.assign(specificaiton, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specificaiton);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
