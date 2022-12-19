311a220d8f06773e760a201dfdfe39b681142145
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-19 10:54:48 -0300

    feat: add courses post route and json middleware

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -2,8 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello, world!' });
+});
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
 });
 
 app.listen(3333, () => console.log('Server running! http://localhost:3333'));
