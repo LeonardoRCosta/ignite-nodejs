8cd633c624e1fe2130a2fd219a1361e2ef38e682
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-19 17:12:38 -0300

    feat: add stream Reading of the imported file.

--- rentalx/src/modules/cars/use-cases/importCategory/ImportCategoryUseCase.ts
+++ rentalx/src/modules/cars/use-cases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File) {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    const parsedFile = parse();
+
+    stream.pipe(parsedFile);
+
+    parsedFile.on('data', async (line) => {
+      console.log(line);
+    });
   }
 }
 
