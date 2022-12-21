228b8c748bf45b3a016b2b8acd9b1a88621d1526
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-21 15:05:18 -0300

    feat: create post user route

---
+++ rentalx/src/routes/users.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { CreateUserController } from '../modules/accounts/use-cases/createUser/CreateUserController';
+
+const usersRoutes = Router();
+
+const createUserController = new CreateUserController();
+
+usersRoutes.post('/', createUserController.handle);
+
+export { usersRoutes };
