db2f796aba52fe29e6964fe087349b6b1ced01a1
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-22 12:32:05 -0300

    feat: uses authentication middleware & multer upload

--- rentalx/src/routes/users.routes.ts
+++ rentalx/src/routes/users.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateUserController } from '../modules/accounts/use-cases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/use-cases/updateUserAvatar/UpdateUserAvatarControler';
 
 const usersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post('/', createUserController.handle);
+
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
