5123f90dabea66cad27eafffb413267b1cd89c00
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-22 12:22:42 -0300

    feat: add middleware to deal with errors.

--- rentalx/src/server.ts
+++ rentalx/src/server.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 import './database';
 import './shared/container';
 import swaggerFile from './swagger.json';
 import { router } from './routes';
+import { AppError } from './errors/AppError';
 
 const app = express();
 
@@ -12,5 +14,18 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({ message: error.message });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: `Internal Server Error: ${error.message}`,
+  });
+
+  next();
+});
 
 app.listen(3333, () => console.log('Server running! http://localhost:3333'));
