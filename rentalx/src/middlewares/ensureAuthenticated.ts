d5924b9c4eb5d4b55a029e4c40265d953c146f21
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-22 12:26:18 -0300

    feat: create ensureAuthenticated middleware

---
+++ rentalx/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+import { AppError } from '../errors/AppError';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
+interface IPayload {
+  sub: string;
+}
+
+export async function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) throw new AppError('Missing Token!', 401);
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      'c807a41fa612b48aefc3bc445a161285'
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = usersRepository.findById(user_id);
+
+    if (!user) throw new AppError('User not found!', 401);
+
+    req.user = {
+      id: user_id,
+    };
+
+    next();
+  } catch {
+    throw new AppError('Invalid Token', 401);
+  }
+}
