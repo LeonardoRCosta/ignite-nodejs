eda2ad91a37f161df8c807375af0ff165c1478c8
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-26 16:33:54 -0300

    test: tests create car use case

---
+++ rentalx/src/modules/cars/use-cases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,65 @@
+import { InMemoryCarsRepository } from '@modules/cars/repositories/in-memory/InMemoryCarsRepository';
+import { AppError } from '@shared/errors/AppError';
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+describe('Create Car', () => {
+  let createCarUseCase: CreateCarUseCase;
+  let inMemoryCarsRepository: InMemoryCarsRepository;
+
+  beforeEach(() => {
+    inMemoryCarsRepository = new InMemoryCarsRepository();
+    createCarUseCase = new CreateCarUseCase(inMemoryCarsRepository);
+  });
+
+  it('should be able to create a car', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Car Name',
+      description: 'Car Description',
+      daily_rate: 100,
+      license_plate: 'abc-1234',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'category',
+    });
+
+    expect(car).toHaveProperty('id');
+  });
+
+  it('should not be able to create a car with license plate already registered', () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Car1',
+        description: 'Car Description',
+        daily_rate: 100,
+        license_plate: 'abc-1234',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'category',
+      });
+
+      await createCarUseCase.execute({
+        name: 'Car2',
+        description: 'Car Description',
+        daily_rate: 100,
+        license_plate: 'abc-1234',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'category',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to create a car with available true by default', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Available Car',
+      description: 'Car Description',
+      daily_rate: 100,
+      license_plate: 'abc-1234',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'category',
+    });
+
+    expect(car.available).toBe(true);
+  });
+});
