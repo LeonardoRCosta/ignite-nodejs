255482681ba74739d029f08a357f1ff51f49cf1b
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-26 16:34:26 -0300

    feat: create CarsRepository singleton

--- rentalx/src/shared/container/index.ts
+++ rentalx/src/shared/container/index.ts
@@ -5,6 +5,8 @@ import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecifica
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -20,3 +22,5 @@ container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
