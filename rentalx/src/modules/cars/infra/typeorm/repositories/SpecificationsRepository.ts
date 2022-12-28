2ad06f6eaa87a9affdca0462c857de50b53cad49
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-28 16:53:36 -0300

    feat: implements findByIds & create returns specification

--- rentalx/src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ rentalx/src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -12,16 +12,25 @@ class SpecificationsRepository implements ISpecificationsRepository {
     this.repository = getRepository(Specification);
   }
 
-  async create({ name, description }: ICreateSpecificationDTO) {
-    const specificaiton = this.repository.create({ description, name });
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
+    const specification = this.repository.create({ description, name });
 
-    await this.repository.save(specificaiton);
+    await this.repository.save(specification);
+
+    return specification;
   }
 
   async findByName(name: string): Promise<Specification> {
     const specification = await this.repository.findOne({ name });
 
     return specification;
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    return await this.repository.findByIds(ids);
   }
 }
 
