47ecaa227103712fc3f2d1f1845cde953b37f222
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-28 16:47:21 -0300

    feat: create entity CarImage

---
+++ rentalx/src/modules/cars/infra/typeorm/entities/CarImage.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { randomUUID } from 'node:crypto';
+
+@Entity('cars_image')
+class CarImage {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  car_id: string;
+
+  @Column()
+  image_name: string;
+
+  @CreateDateColumn()
+  created_at: string;
+
+  constructor() {
+    if (!this.id) this.id = randomUUID();
+  }
+}
+
+export { CarImage };
