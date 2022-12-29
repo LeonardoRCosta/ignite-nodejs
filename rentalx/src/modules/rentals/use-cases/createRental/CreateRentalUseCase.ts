3bbe8c5257bf59e212c571133532020dc18796db
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-29 16:02:26 -0300

    feat: create CreateRentalUseCase

---
+++ rentalx/src/modules/rentals/use-cases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,38 @@
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { AppError } from '@shared/errors/AppError';
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+  constructor(private rentalsRepository: IRentalsRepository) {}
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+    const carUnavailable = await this.rentalsRepository.findRentalByCarId(
+      car_id
+    );
+
+    if (carUnavailable) throw new AppError('This car is unavailable');
+
+    const user = await this.rentalsRepository.findRentalByUserId(user_id);
+
+    if (user) throw new AppError('This user has an in progress rental');
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental;
+  }
+}
+
+export { CreateRentalUseCase };
