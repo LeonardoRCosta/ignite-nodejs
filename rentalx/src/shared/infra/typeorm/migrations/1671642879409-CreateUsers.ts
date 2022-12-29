d893f009766dae36833e24425680a8b438164fcf
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-29 15:20:37 -0300

    refactor: add isPrimary at id column

--- rentalx/src/shared/infra/typeorm/migrations/1671642879409-CreateUsers.ts
+++ rentalx/src/shared/infra/typeorm/migrations/1671642879409-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1671642879409 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
