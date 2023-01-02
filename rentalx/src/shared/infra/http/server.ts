00f2a6b525b7312130be28e7575f28b3f501870f
LeonardoRCosta <rm94286@fiap.com.br>
2023-01-02 11:03:46 -0300

    refactor: server file just runs the app

--- rentalx/src/shared/infra/http/server.ts
+++ rentalx/src/shared/infra/http/server.ts
@@ -1,33 +1,3 @@
-import express, { NextFunction, Request, Response } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import 'express-async-errors';
-
-import createConnection from '@shared/infra/typeorm';
-import '@shared/container';
-import swaggerFile from '../../../swagger.json';
-import { AppError } from '@shared/errors/AppError';
-import { router } from '@shared/infra/http/routes';
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-app.use(router);
-
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({ message: error.message });
-  }
-
-  return res.status(500).json({
-    status: 'error',
-    message: `Internal Server Error: ${error.message}`,
-  });
-
-  next();
-});
+import { app } from './app';
 
 app.listen(3333, () => console.log('Server running! http://localhost:3333'));
