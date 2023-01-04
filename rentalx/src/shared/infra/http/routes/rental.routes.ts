8239a5d7ad9d191354c973f853a46fda8926f1c3
LeonardoRCosta <rm94286@fiap.com.br>
2023-01-04 12:44:21 -0300

    feat: create rental return route

--- rentalx/src/shared/infra/http/routes/rental.routes.ts
+++ rentalx/src/shared/infra/http/routes/rental.routes.ts
@@ -1,11 +1,19 @@
 import { CreateRentalController } from '@modules/rentals/use-cases/createRental/CreateRentalController';
+import { RentalReturnController } from '@modules/rentals/use-cases/rentalReturn/RentalReturnController';
 import { Router } from 'express';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
+const rentalReturnController = new RentalReturnController();
 
 rentalRoutes.post('/', ensureAuthenticated, createRentalController.handle);
+
+rentalRoutes.post(
+  '/return/:id',
+  ensureAuthenticated,
+  rentalReturnController.handle
+);
 
 export { rentalRoutes };
