1bdb900b87c937865b38149311f882eda0b3c96e
LeonardoRCosta <rm94286@fiap.com.br>
2023-01-09 15:46:17 -0300

    feat: add password reset route

--- rentalx/src/shared/infra/http/routes/password.routes.ts
+++ rentalx/src/shared/infra/http/routes/password.routes.ts
@@ -1,10 +1,13 @@
+import { ResetUserPasswordController } from '@modules/accounts/use-cases/resetUserPassword/ResetUserPasswordController';
 import { SendForgotPasswordMailContoller } from '@modules/accounts/use-cases/sendForgotPasswordMail/SendForgotPasswordMailController';
 import { Router } from 'express';
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailContoller = new SendForgotPasswordMailContoller();
+const resetUserPasswordController = new ResetUserPasswordController();
 
 passwordRoutes.post('/forgot', sendForgotPasswordMailContoller.handle);
+passwordRoutes.post('/reset', resetUserPasswordController.handle);
 
 export { passwordRoutes };
