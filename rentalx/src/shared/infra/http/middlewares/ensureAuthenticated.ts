863b55611ad94c249eb4aabfc4bddaaf052f9951
LeonardoRCosta <rm94286@fiap.com.br>
2023-01-09 10:58:38 -0300

    refactor: expects refresh token instead of token

--- rentalx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ rentalx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,7 +1,8 @@
+import auth from '@config/auth';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
+import { AppError } from '@shared/errors/AppError';
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
-import { AppError } from '@shared/errors/AppError';
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 
 interface IPayload {
   sub: string;
@@ -13,6 +14,7 @@ export async function ensureAuthenticated(
   next: NextFunction
 ) {
   const authHeader = req.headers.authorization;
+  const usersTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) throw new AppError('Missing Token!', 401);
 
@@ -21,12 +23,13 @@ export async function ensureAuthenticated(
   try {
     const { sub: user_id } = verify(
       token,
-      'c807a41fa612b48aefc3bc445a161285'
+      auth.secret_refresh_token
     ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-
-    const user = usersRepository.findById(user_id);
+    const user = usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
 
     if (!user) throw new AppError('User not found!', 401);
 
