a2f1522db984e7ea738dd6e03e781c0e0ba1c69d
LeonardoRCosta <rm94286@fiap.com.br>
2023-01-09 10:48:25 -0300

    feat: create auth config file

---
+++ rentalx/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: 'c807a41fa612b48aefc3bc445a161285',
+  token_expires_in: '15min',
+  secret_refresh_token: 'a9cda118c5ac097707ee40dcc8f777cd',
+  refresh_token_expires_in: '30d',
+  refresh_token_expires_in_days: 30,
+};
