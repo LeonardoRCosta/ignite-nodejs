2c6de7d63e78db79ee38e57167e4238e67a4a5d4
LeonardoRCosta <rm94286@fiap.com.br>
2023-01-09 10:57:28 -0300

    feat: add UsersTokensRepository singleton

--- rentalx/src/shared/container/index.ts
+++ rentalx/src/shared/container/index.ts
@@ -2,18 +2,20 @@ import { container } from 'tsyringe';
 
 import '@shared/container/providers';
 
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
-import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
-import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
-import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
-import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository';
-import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
-import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { RentalsRepository } from '@modules/rentals/infra/typeorm/repositories/RentalsRepository';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -40,4 +42,9 @@ container.registerSingleton<ICarsImagesRepository>(
 container.registerSingleton<IRentalsRepository>(
   'RentalsRepository',
   RentalsRepository
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+  'UsersTokensRepository',
+  UsersTokensRepository
 );
