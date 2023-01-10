34d2096a5cec2d50bda3e9dbadd108b9e2af2c1d
LeonardoRCosta <rm94286@fiap.com.br>
2023-01-10 14:48:47 -0300

    refactor: expecting token instead of refresh_token

--- rentalx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ rentalx/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,4 @@
 import auth from '@config/auth';
-import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import { AppError } from '@shared/errors/AppError';
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
@@ -14,24 +13,13 @@ export async function ensureAuthenticated(
   next: NextFunction
 ) {
   const authHeader = req.headers.authorization;
-  const usersTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) throw new AppError('Missing Token!', 401);
 
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      auth.secret_refresh_token
-    ) as IPayload;
-
-    const user = usersTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token
-    );
-
-    if (!user) throw new AppError('User not found!', 401);
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
     req.user = {
       id: user_id,
