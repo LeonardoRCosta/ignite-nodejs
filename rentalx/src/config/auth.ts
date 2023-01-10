33fb71066dd70cf6fe59e752a107e328624fae3b
LeonardoRCosta <rm94286@fiap.com.br>
2023-01-10 14:42:25 -0300

    refactor: changes token duration

--- rentalx/src/config/auth.ts
+++ rentalx/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   secret_token: 'c807a41fa612b48aefc3bc445a161285',
-  token_expires_in: '15min',
+  token_expires_in: '5min',
   secret_refresh_token: 'a9cda118c5ac097707ee40dcc8f777cd',
   refresh_token_expires_in: '30d',
   refresh_token_expires_in_days: 30,
