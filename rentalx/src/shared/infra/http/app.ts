fc40173e9d843f8e990cf332eefad23c871f3645
LeonardoRCosta <rm94286@fiap.com.br>
2023-01-10 14:48:11 -0300

    fix: imports dotenv/config

--- rentalx/src/shared/infra/http/app.ts
+++ rentalx/src/shared/infra/http/app.ts
@@ -1,12 +1,14 @@
+import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
-import swaggerUi from 'swagger-ui-express';
 import 'express-async-errors';
+import 'reflect-metadata';
+import swaggerUi from 'swagger-ui-express';
 
-import createConnection from '@shared/infra/typeorm';
 import '@shared/container';
-import swaggerFile from '../../../swagger.json';
 import { AppError } from '@shared/errors/AppError';
 import { router } from '@shared/infra/http/routes';
+import createConnection from '@shared/infra/typeorm';
+import swaggerFile from '../../../swagger.json';
 
 createConnection();
 const app = express();
